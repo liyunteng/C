f15b7f033d5df784368816457a4e52ab76baa235
liyunteng <li_yunteng@163.com>
2017-06-18 21:02:36 +0800

    add ffmpeg/codec

--- ffmpeg/data/pcm.c
+++ ffmpeg/data/pcm.c
@@ -6,10 +6,11 @@
  *
  * Author: liyunteng <liyunteng@streamocean.com>
  * License: StreamOcean
- * Last-Updated: 2017/06/16 17:55:46
+ * Last-Updated: 2017/06/17 00:06:08
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int split_pcm16le(char *url)
 {
@@ -109,5 +110,92 @@ int pcm16le_to_pcm8(char *url)
     free(sample);
     fclose(fp);
     fclose(fp1);
+    return 0;
+}
+
+int pcm16le_to_wave(const char *pcmpath, int channels, int sample_rate, consta char *wavepath)
+{
+#pragma pack(1)
+    typedef struct {
+        char fccID[4];
+        unsigned long dwSize;
+        char fccType[4];
+    } WAVE_HEADER;
+
+    typedef struct {
+        char fccID[4];
+        unsigned long dwSize;
+        unsigned short wFormatTag;
+        unsigned short wChannels;
+        unsigned long dwSamplesPerSec;
+        unsigned long dwAvgBytesPerSec;
+        unsigned short wBlockAlign;
+        unsigned short uiBitsPerSample;
+    } WAVE_FMT;
+
+    typedef struct {
+        char fccID[4];
+        unsigned long dwSize;
+    } WAVE_DATA;
+
+
+    if (channels == 0 || sample_rate == 0) {
+        channels = 2;
+        sample_rate = 44100;
+    }
+
+    int bits = 16;
+
+    WAVE_HEADER pcmHEADER;
+    WAVE_FMT pcmFMT;
+    WAVE_DATA pcmDATA;
+
+    unsigned short m_pcmData;
+    FILE *fp, *fpout;
+
+    fp = fopen(pcmpath, "rb");
+    if (fp == NULL) {
+        return -1;
+    }
+    fpout = fopen(wavepath, 'wb+');
+    if (fpout == NULL) {
+        return -1;
+    }
+
+    memcpy(pcmHEADER.fccID, "RIFF", strlen("RIFF"));
+    memcpy(pcmHEADER.fccType, "WAVE", strlen("WAVE"));
+    fseek(fpout, sizeof(WAVE_HEADER), 1);
+
+    pcmFMT.dwSamplesPerSec = sample_rate;
+    pcmFMT.dwAvgBytesPerSec = pcmFMT.dwSamplesPerSec * sizeof(m_pcmData);
+    pcmFMT.uiBitsPerSample = bits;
+    memcpy(pcmFMT.fccID, "fmt ", strlen("fmt "));
+    pcmFMT.dwSize = 16;
+    pcmFMT.wBlockAlign = 2;
+    pcmFMT.wChannels = channels;
+    pcmFMT.wFormatTag = 1;
+    fwrite(&pcmFMT, sizeof(WAVE_FMT), 1, fpout);
+
+    memcpy(pcmDATA.fccID, "data", strlen("data"));
+    pcmDATA.dwSize  = 0;
+    fseek(fpout, sizeof(WAVE_DATA), SEEK_CUR);
+
+    fread(&m_pcmData, sizeof(unsigned short), 1, fp);
+    while (!feof(fp)) {
+        pcmDATA.dwSize += 2;
+        fwrite(&m_pcmData, sizeof(unsigned short), 1, fpout);
+        fread(&m_pcmData, sizeof(unsigned short), 1, fp);
+    }
+
+    pcmHEADER.dwSize = 44 + pcmDATA.dwSize;
+
+    rewind(fpout);
+    fwrite(&pcmHEADER, sizeof(WAVE_HEADER), 1, fpout);
+    fseek(fpout, sizeof(WAVE_FMT), SEEK_CUR);
+    fwrite(&pcmDATA, sizeof(WAVE_DATA), 1, fpout);
+
+    fclose(fp);
+    fclose(fpout);
+
     return 0;
 }
